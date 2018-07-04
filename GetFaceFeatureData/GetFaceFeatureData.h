7cb5cfd83bffca414cc4ab61ee7f6e6c87354297
Alyson Chen <qoo@alysons-macbook-pro.local>
2018-07-04 11:52:51 +0800

    Initial Commit

---
+++ GetFaceFeatureData/GetFaceFeatureData.h
@@ -0,0 +1,13 @@
+//
+//  GetFaceFeatureData.h
+//  GetFaceFeatureData
+//
+//  Created by Alyson Chen on 2018/7/4.
+//  Copyright © 2018年 Alyson Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GetFaceFeatureData : NSObject
+
+@end
---
+++ GetFaceFeatureData/GetFaceFeatureData.m
@@ -0,0 +1,13 @@
+//
+//  GetFaceFeatureData.m
+//  GetFaceFeatureData
+//
+//  Created by Alyson Chen on 2018/7/4.
+//  Copyright © 2018年 Alyson Chen. All rights reserved.
+//
+
+#import "GetFaceFeatureData.h"
+
+@implementation GetFaceFeatureData
+
+@end
